994be917992e4a691aeb459ae383e3fc7796a51d
abe-almar <abe.alcarazm@gmail.com>
2025-01-19 14:32:36 +0100

    added language selector and translations.js

---
+++ translations.js
@@ -0,0 +1,16 @@
+const translations = {
+    es: {
+        welcome: "Bienvenido",
+        know_more: "Quiero saber más",
+        transform_data: "Transformamos Datos en Decisiones Estratégicas",
+        contact_us: "Contáctanos",
+        start_now: "Empieza ahora",
+    },
+    en: {
+        welcome: "Welcome",
+        know_more: "Learn More",
+        transform_data: "We Turn Data Into Strategic Decisions",
+        contact_us: "Contact Us",
+        start_now: "Start Now",
+    },
+};
